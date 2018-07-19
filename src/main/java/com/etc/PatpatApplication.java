7f29da7d2a13a241b85e03e425d6a849d2a5488c
Administrator <Administrator@stu14006>
2018-07-19 16:09:39 +0800

    项目创建

---
+++ src/main/java/com/etc/PatpatApplication.java
@@ -0,0 +1,12 @@
+package com.etc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatpatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatpatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/etc/PatpatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.etc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatpatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
