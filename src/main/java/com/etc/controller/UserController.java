a50a131825fb8f6fed8521e3b007745c7365ce39
NogardWolley <1981175925@qq.com>
2018-07-23 12:40:35 +0800

    userController完善功能

--- src/main/java/com/etc/controller/UserController.java
+++ src/main/java/com/etc/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.etc.controller;
 
-import com.etc.entity.JsonResult;
-import com.etc.entity.User;
+import com.etc.entity.*;
 import com.etc.enums.ErrorEnum;
 import com.etc.exception.MyException;
+import com.etc.service.AppFollowService;
+import com.etc.service.AppService;
+import com.etc.service.UserFollowService;
 import com.etc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,11 +13,20 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private AppFollowService appFollowService;
+    @Autowired
+    private AppService appService;
+    @Autowired
+    private UserFollowService userFollowService;
+
     @GetMapping("/signin")
     public JsonResult signin(String userName,String passWord){
         int i=userService.login(userName,passWord);
@@ -50,16 +61,46 @@ public class UserController {
         return new JsonResult<User>(userService.selectUserByName(userName));
     }
     //用户关注用户
-    @PutMapping("/user/{userName}/follow/{followUserName}")
+    @PutMapping("/user/{userName}/followUser/{followUserName}")
     public JsonResult userFollow(@PathVariable String userName,@PathVariable String followUserName){
-        if(userService.userFollowUser(userName,followUserName))return new JsonResult("关注成功");
-        else return new JsonResult("关注失败");
+        if(userService.userFollowUser(userName,followUserName))return new JsonResult("用户关注成功");
+        else return new JsonResult("用户关注失败");
     }
     //用户取关用户
-    @DeleteMapping("/user/{userName}/disfollow/{followUserName}")
+    @DeleteMapping("/user/{userName}/disfollowUser/{followUserName}")
     public JsonResult userDisFollow(@PathVariable String userName,@PathVariable String followUserName){
-        if(userService.userDisfollowUser(userName,followUserName))return new JsonResult("取关成功");
-        else return new JsonResult("取关失败");
+        if(userService.userDisfollowUser(userName,followUserName))return new JsonResult("用户取关成功");
+        else return new JsonResult("用户取关失败");
+    }
+    //获取用户关注用户列表
+    @GetMapping("/user/{userName}/userFollowList")
+    public JsonResult<User> getUserFollowUserList(@PathVariable String userName){
+        List<User> userList=new ArrayList<User>();
+        for(UserFollow userFollow:userFollowService.getUserFollowList(userName)){
+            userList.add(userService.otherSelectUserById(userFollow.getFollowuserid()));
+        }
+        return new JsonResult<User>(userList);
+    }
+    //用户关注应用
+    @PutMapping("/user/{userName}/followApp/{appId}")
+    public JsonResult userFollowApp(@PathVariable String userName,@PathVariable int appId){
+        if(userService.userFollowApp(userName,appId))return new JsonResult("App关注成功");
+        else return new JsonResult("App关注失败");
+    }
+    //用户取关应用
+    @DeleteMapping("/user/{userName}/disfollowApp/{appId}")
+    public JsonResult userDisfollowApp(@PathVariable String userName,@PathVariable int appId){
+        if(userService.userDisfollowApp(userName,appId))return new JsonResult("App取关成功");
+        else return new JsonResult("App取关失败");
+    }
+    //获取用户关注App列表
+    @GetMapping("/user/{userName}/appFollowList")
+    public JsonResult<App> getUserFollowAppList(@PathVariable String userName){
+        List<App> appList=new ArrayList<App>();
+        for(AppFollow appFollow:appFollowService.getAppFollowList(userName)){
+            appList.add(appService.selectAppById(appFollow.getFollowappid()));
+        }
+        return new JsonResult<App>(appList);
     }
     //
     //测试
