27adf459265b0051a89c9ed047b11cb184587149
HarryDongH <37304036+HarryDongH@users.noreply.github.com>
2018-07-24 21:56:57 +0800

    按照发布时间排序和分类排序

--- src/main/java/com/etc/controller/AppController.java
+++ src/main/java/com/etc/controller/AppController.java
@@ -107,7 +107,59 @@ public class AppController {
             apps1.add(apps.get(s[apps.size()-i-1][1]));
         return apps1;
     }
-    
+
+    @ResponseStatus(value= HttpStatus.OK)
+    @RequestMapping(value = "/app/sort/type",method = RequestMethod.GET)
+    @ResponseBody
+    public List<App> sortAppByType(@RequestParam String type){
+        List<App> apps=appService.selectAppByType(type);
+        int[][] s=new int[apps.size()][2];
+        for(int i=0;i<apps.size();i++) {
+            int j=Integer.valueOf(apps.get(i).getAppdownloads());
+            s[i][0] = (int)(apps.get(i).getAppscore()*10);
+            while(j!=0){
+                s[i][0]++;
+                j/=10;
+            }
+            s[i][1]=i;
+        }
+        sortByDownload(s,0,apps.size()-1);
+        List<App> apps1=new ArrayList<>();
+        for(int i=0;i<apps.size();i++)
+            apps1.add(apps.get(s[apps.size()-i-1][1]));
+        return apps1;
+    }
+
+
+    @ResponseStatus(value= HttpStatus.OK)
+    @RequestMapping(value = "/app/sort/time",method = RequestMethod.GET)
+    @ResponseBody
+    public List<App> sortAppByTime(){
+        List<App> apps=appService.selectAllApp();
+        List<App> apps1=new ArrayList<>();
+        Calendar calendar=Calendar.getInstance();
+        if(calendar.get(Calendar.MONTH)-2>=0)
+            calendar.set(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH)-2,calendar.get(Calendar.DATE),calendar.get(Calendar.HOUR),calendar.get(Calendar.MINUTE),calendar.get(Calendar.SECOND));
+        else
+            calendar.set(calendar.get(Calendar.YEAR)-1,calendar.get(Calendar.MONTH)+10,calendar.get(Calendar.DATE),calendar.get(Calendar.HOUR),calendar.get(Calendar.MINUTE),calendar.get(Calendar.SECOND));
+        Date date=calendar.getTime();
+        for(int i=0;i<apps.size();i++){
+            if(apps.get(i).getAppdate().after(date))apps1.add(apps.get(i));
+        }
+
+        int[][] s=new int[apps.size()][2];
+        for(int i=0;i<apps1.size();i++) {
+            s[i][0] = Integer.valueOf(apps1.get(i).getAppdownloads())+(int)(apps1.get(i).getAppscore()*10);
+            s[i][1]=i;
+        }
+        sortByDownload(s,0,apps1.size()-1);
+        List<App> apps2=new ArrayList<>();
+        for(int i=0;i<apps1.size();i++)
+            apps2.add(apps1.get(s[apps1.size()-i-1][1]));
+        return apps2;
+
+    }
+
 
     public void sortByDownload(int[][] apps,int start,int end){
         if(start<end){
